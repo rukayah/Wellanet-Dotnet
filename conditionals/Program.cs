4c9761d3233f1af9368369f823fa8b8b01dddf6c
Rukayah <ruqqy.adesoye@gmail.com>
2021-01-05 20:19:23 +0100

    conditionals

---
+++ conditionals/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            Console.WriteLine("Welcome, how old are you");
+            age = int.Parse(Console.ReadLine());
+
+            if(age >= 18)
+            {
+                Console.WriteLine("You are old enough to drink");
+            }
+            else
+            {
+                Console.WriteLine("You should never drink");
+            }
+        }
+    }
+}
