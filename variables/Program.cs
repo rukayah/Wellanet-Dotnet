38b81e7fdb422dee903a061054d86edea3e5aa66
Rukayah <ruqqy.adesoye@gmail.com>
2021-01-05 20:12:30 +0100

    add variable

---
+++ variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string personName = " ";
+            Console.Write("Please enter your name: ");
+
+            personName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + personName);
+        }
+    }
+}
+
