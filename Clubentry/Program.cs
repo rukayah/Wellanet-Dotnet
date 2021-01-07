33648f995406d3ca6cf1f3ba16a5a2db5197c8bb
Rukayah <ruqqy.adesoye@gmail.com>
2021-01-07 07:43:00 +0100

    Club entry app

---
+++ Clubentry/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Clubentry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int susanAge, christianaAge, joelAge;
+
+            Console.WriteLine("Welcome susan, how old are you please?");
+            susanAge = int.Parse(Console.ReadLine());
+
+            if(susanAge >= 18)
+            {
+                Console.WriteLine("You are welcome to our club Susan");
+            } 
+            else 
+            {
+                Console.WriteLine("You are not allowed in the club");
+            }
+
+            Console.WriteLine("Welcome christiana, how old are you please?");
+            christianaAge = int.Parse(Console.ReadLine());
+
+            if(christianaAge >= 18)
+            {
+                Console.WriteLine("You are welcome to our club Christiana");
+            } 
+            else 
+            {
+                Console.WriteLine("You are not allowed in the club");
+            }
+            Console.WriteLine("Welcome joel, how old are you please?");
+            joelAge = int.Parse(Console.ReadLine());
+
+            if(joelAge >= 18)
+            {
+                Console.WriteLine("You are welcome to our club Joel");
+            } 
+            else 
+            {
+                Console.WriteLine("You are not allowed in the club");
+            }
+        }
+    }
+}
