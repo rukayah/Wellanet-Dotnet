037c2bb251e6e1aabe63904d3fd6750f5079da63
Rukayah <ruqqy.adesoye@gmail.com>
2021-01-10 17:55:01 +0100

    Good

---
+++ Guessinggame/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Guessinggame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = " ";
+            Console.Write("Hello! What is your name");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hi " + name + "Let us play a guessing game");
+
+
+           
+
+
+
+
+        }
+    }
+}
